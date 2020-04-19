79019b2570b9667f0ad699d36760ea071ee85fca
Wilson <p780729@hotmail.com>
2020-04-19 21:37:34 +0800

    Add home and new feature ( CR of CRUD)

--- routes/records.js
+++ routes/records.js
@@ -1,45 +1,68 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../models/record')
+const db = require('../models')
+const Record = db.Record
+const User = db.User
 const { authenticated } = require('../config/auth')
 
 // 設定路由
 // 列出全部 & filter
 router.get('/', authenticated, (req, res) => {
-  const month = req.query.month || ''
-  const category = req.query.category || ''
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
 
-  let categoryRegex = new RegExp('')
-  if (category) {
-    categoryRegex = new RegExp(category)
-  } else {
-    categoryRegex = new RegExp('[a-zA-Z]')
-  }
+      return Record.findAll({
+        raw: true,
+        nest: true,
+        where: {
+          userId: req.user.id
+        }
+      })
+    })
+    .then((records) => {
+      // console.log(records)
+      const month = req.query.month || ''
+      const category = req.query.category || ''
 
-  let totalAmount = 0
-
-  // Record.find({ userId: req.user._id })
-  Record.find({ userId: req.user._id, category: { $regex: categoryRegex } })
-    .collation({ locale: 'en_US' }) // 設定英文語系
-    .lean()
-    .exec((err, records) => {
-      if (err) return console.log(err)
       // 轉換 date 格式給前端使用
       records.forEach((record) => {
-        record.date = record.date.toISOString().slice(0, 10) // 把日期 Date 格式 轉成字串 xxxx-xx-xx 的格式
+        record.date = record.date.toLocaleString().split('-')
+        record.date[2] = record.date[2].slice(0, 2).replace(' ', '')
+        if (record.date[1].length === 1) {
+          record.date[1] = '0' + record.date[1]
+        }
+        if (record.date[2].length === 1) {
+          record.date[2] = '0' + record.date[2]
+        }
+        record.date = record.date.join('-')
       })
+
+      // 篩選類別
+      if (category) {
+        records = records.filter((record) => {
+          return (record.category === category)
+        })
+      }
+
       // 篩選月份
       if (month) {
         records = records.filter((record) => {
-          return record.date.slice(5, 7).includes(month)
+          return record.date.slice(4, 7).includes(month)
         })
       }
+
       // 計算總金額
+      let totalAmount = 0
       records.forEach((record) => {
         totalAmount += record.amount
       })
 
       return res.render('index', { records, month, category, totalAmount })
+    })
+    .catch((err) => {
+      console.log(err)
+      return res.status(422).json(err)
     })
 })
 
@@ -47,14 +70,14 @@ router.get('/', authenticated, (req, res) => {
 router.get('/new', authenticated, (req, res) => {
   let today = new Date()
   today = today.toISOString().slice(0, 10)
-  console.log('today', today)
   return res.render('new', { today })
 })
 
 // 新增一筆 record
 router.post('/', authenticated, (req, res) => {
-  req.body.userId = req.user._id
-  const record = new Record(req.body)
+  req.body.UserId = req.user.id
+  const record = req.body
+  console.log('record', record)
 
   const errors = []
   if (!record.name || !record.amount || !record.category || !record.date) {
@@ -62,18 +85,29 @@ router.post('/', authenticated, (req, res) => {
   }
 
   if (errors.length > 0) {
-    return res.render('new', { record, errors })
+    let today = new Date()
+    today = today.toISOString().slice(0, 10)
+    return res.render('new', { record, today, errors })
   }
 
-  record.save(err => {
-    if (err) return console.log(err)
-    return res.redirect('/')
+  Record.create({
+    name: req.body.name,
+    amount: req.body.amount,
+    category: req.body.category,
+    date: req.body.date,
+    merchant: req.body.merchant,
+    UserId: req.body.UserId
   })
+    .then((record) => { return res.redirect('/') })
+    .catch((err) => {
+      console.log(err)
+      return res.status(422).json(err)
+    })
 })
 
 // 顯示修改 record 頁面
 router.get('/:id/edit', authenticated, (req, res) => {
-  Record.findOne({ _id: req.params.id, userId: req.user._id })
+  Record.findOne({ id: req.params.id, userId: req.user.id })
     .lean()
     .exec((err, record) => {
       if (err) return console.log(err)
@@ -84,7 +118,7 @@ router.get('/:id/edit', authenticated, (req, res) => {
 
 // 修改 record
 router.put('/:id', authenticated, (req, res) => {
-  req.body.userId = req.user._id
+  req.body.userId = req.user.id
 
   const errors = []
   if (!req.body.name || !req.body.amount || !req.body.category || !req.body.date) {
@@ -92,7 +126,7 @@ router.put('/:id', authenticated, (req, res) => {
   }
 
   if (errors.length > 0) {
-    Record.findOne({ _id: req.params.id, userId: req.user._id })
+    Record.findOne({ id: req.params.id, userId: req.user.id })
       .lean()
       .exec((err, record) => {
         if (err) return console.log(err)
@@ -100,10 +134,10 @@ router.put('/:id', authenticated, (req, res) => {
         return res.render('edit', { record, errors })
       })
   } else {
-    Record.findOne({ _id: req.params.id, userId: req.user._id }, (err, record) => {
+    Record.findOne({ id: req.params.id, userId: req.user.id }, (err, record) => {
       if (err) return console.log(err)
 
-      req.body.userId = req.user._id
+      req.body.userId = req.user.id
       Object.assign(record, req.body)
 
       record.save(err => {
@@ -116,7 +150,7 @@ router.put('/:id', authenticated, (req, res) => {
 
 // 刪除 record
 router.delete('/:id/delete', authenticated, (req, res) => {
-  Record.findOne({ _id: req.params.id, userId: req.user._id }, (err, record) => {
+  Record.findOne({ id: req.params.id, userId: req.user.id }, (err, record) => {
     if (err) return console.log(err)
     record.remove(err => {
       if (err) return console.log(err)
